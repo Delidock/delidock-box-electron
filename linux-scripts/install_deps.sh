24be712fddf764f7d37915dc0c00fdb8e785d8f3
StepSkop <skopek.stepan@gmail.com>
2024-04-04 21:08:28 +0200

    feat: install script

--- linux-scripts/install_deps.sh
+++ linux-scripts/install_deps.sh
@@ -1,2 +1,9 @@
 #!/bin/bash
 sudo apt install util-linux procps hostapd iproute2 haveged dnsmasq iptables
+echo 'ALL ALL=NOPASSWD: /usr/bin/create_ap' | sudo EDITOR='tee -a' visudo -f /etc/sudoers.d/create_ap
+sudo cp create_ap /usr/bin
+mkdir $HOME/.config/autostart
+cp delidock.desktop $HOME/.config/autostart
+
+echo 'SUBSYSTEM=="net", ACTION=="add", ATTR{address}=="a8:42:a1:ed:da:c8", NAME="wifi0"' | sudo EDITOR='tee -a' visudo -f /etc/udev/rules.d/10-network-device.rules
+echo 'SUBSYSTEM=="net", ACTION=="add", ATTR{address}=="d8:3a:dd:37:84:35", NAME="ap0"' | sudo EDITOR='tee -a' visudo -f /etc/udev/rules.d/10-network-device.rules
